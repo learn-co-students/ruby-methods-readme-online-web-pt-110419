c39e9da63013af34530e170cf4aab9c9cb53b4de
Nikelson Exantus <nikelsone1981@hotmail.com>
2019-11-06 00:42:43 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -11,3 +11,33 @@ puts phrase
 puts phrase
 puts phrase
 puts phrase
+# ... The rest of the program
+
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+puts phrase
+
+def say_hello_world_ten_times
+  phrase = "Hello World!"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
+say_hello_world_ten_times
+# ... The rest of the program
+
+say_hello_world_ten_times
