c5da0bfbb99e493317cd105af00973dc3eb9a337
Ronald Taylor <ronald.taylorjr1986@gmail.com>
2019-11-11 19:35:06 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,15 @@
-# Use this space to code along with the readme
-
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
+def say_hello_world_ten_times
+puts 'Hello World'
+phrase = 'Hello World'
+def say_hello_world_ten_times
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
